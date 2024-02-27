2d7a9de9135ed04699fc48a4a9b16cfda1c2d681
Al_27 <ahmedlaghfour@gmail.com>
2024-02-27 02:04:01 +0000

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,21 +9,25 @@ from api.v1.views import *
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-hst = "0.0.0.0" if env.get("HBNB_API_HOST") is None else env.get("HBNB_API_HOST")
+hst = "0.0.0.0" if env.get(
+    "HBNB_API_HOST") is None else env.get("HBNB_API_HOST")
 prt = "5000" if env.get("HBNB_API_PORT") is None else env.get("HBNB_API_PORT")
+
 
 @app.errorhandler(404)
 def route_notfound(e):
-    return jsonify({ "error": "Not found" }),404
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(400)
 def route_error400(e):
-    return jsonify(e.description),400
+    return jsonify(e.description), 400
+
 
 @app.teardown_appcontext
 def teardown(s):
     models.storage.close()
-    
+
+
 if __name__ == "__main__":
     app.run(host=hst, port=prt)
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,10 +4,11 @@
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 import models
- 
 
-@app_views.route("/users", methods=["GET","POST"],strict_slashes=False)
-@app_views.route("/users/<user_id>", methods=["GET","DELETE","PUT"],strict_slashes=False)
+
+@app_views.route("/users", methods=["GET", "POST"], strict_slashes=False)
+@app_views.route("/users/<user_id>",
+                 methods=["GET", "DELETE", "PUT"], strict_slashes=False)
 def users_route(user_id=None):
     """ str """
     if user_id is None:
@@ -18,42 +19,42 @@ def users_route(user_id=None):
             return jsonify(users)
         else:
             try:
-                req = request.get_json()  
-            except Exception as e :
-                abort(400,{"error":"Not a JSON"})
-            
+                req = request.get_json()
+            except Exception as e:
+                abort(400, {"error": "Not a JSON"})
+
             if "email" not in req.keys():
-                abort(400,{"error":"Missing email"})
+                abort(400, {"error": "Missing email"})
             if "password" not in req.keys():
-                abort(400,{"error":"Missing password"})
-            
+                abort(400, {"error": "Missing password"})
+
             user = models.user.User(**req)
             models.storage.new(user)
             models.storage.save()
-            
+
             return jsonify(user.to_dict()), 201
     else:
         user_id = f'User.{user_id}'
         user = models.storage.all(models.user.User).get(user_id)
-        
+
         if user is None:
             abort(404)
-        
+
         if request.method == "GET":
             return jsonify(user.to_dict())
         elif request.method == "PUT":
             try:
-                req = request.get_json()  
-            except Exception as e :
-                abort(400,{"error":"Not a JSON"}) 
-            
-            for k,v in req.items():
-                user.__setattr__(k,v)
+                req = request.get_json()
+            except Exception as e:
+                abort(400, {"error": "Not a JSON"})
+
+            for k, v in req.items():
+                user.__setattr__(k, v)
             user.save()
-            
+
             return jsonify(user.to_dict()), 200
         else:
             models.storage.delete(user)
             models.storage.save()
-            
+
     return jsonify({}), 200
